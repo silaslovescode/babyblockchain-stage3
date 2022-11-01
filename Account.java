9371d148d84ed2f90b2b8dadfcda379804f913f4
silas magu <107644537+silaslovescode@users.noreply.github.com>
2022-11-01 10:27:41 +0300

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,47 @@
+package BABY-BLOCKCHAIN;
+
+import Integer;
+import java.util.Vector;
+import String;
+
+public class Account extends Hash, Hash, , KeyPair {
+
+  public String accountID;
+
+  public KeyPair;
+
+  public Integer balance ;
+
+    public Vector  myOperation;
+
+  public void public static Account genAccount() {
+  }
+
+  public void public void addKeyPairToWallet() {
+  }
+
+  public void public void updateBalance() {
+  }
+
+  public void public int getBalance() {
+  }
+
+  public void public void printBalance() {
+  }
+
+  public void public byte[] signData() {
+  }
+
+  public void public Operation create Operation() {
+  }
+
+  public void public String toString() {
+  }
+
+  public void public void print() {
+  }
+
+  public void newOperation() {
+  }
+
+}
